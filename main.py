bcc446f130a2701674f7277dd18cfe93c93c9908
221RDB153 <127057841+221RDB153@users.noreply.github.com>
2023-03-20 01:04:59 +0000

    v4

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def main():
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
     result = parallel_processing(n, m, data)
-    for threads, time in result:
-        print(threads, time)
+    for thread, start_time in result:
+        print(thread, start_time)
 
 if __name__ == "__main__":
     main()
