931fa4d7366a06cab6dc1c5c01d4e4941a859ffe
221RDB153 <127057841+221RDB153@users.noreply.github.com>
2023-03-20 00:57:17 +0000

    v2

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 # python3
-import heapq
 
 def parallel_processing(n, m, data):
     output = []
-    heap =[(0, i) for i in range(n)]
-    heap.heapify(heap)
-    for i in range(m):
-        job_time = data[i]
-        thread_time, thread_index = heapq.heappop(heap)
-        output.append((thread_index, thread_time))
-        thread_time += job_time
-        heapq.heappush(heap,(thread_time, thread_index))
+    jobs =[(data[i], i) for i in range(n)]
+    threads = [0]*n
+    for job in jobs:
+        thread = threads.index(min(threads))
+        start_time = threads[thread]
+        output.append((thread, start_time))
+        threads[thread] += job[0]
     return output
 
 def main():
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
     result = parallel_processing(n, m, data)
-    for thread_index, start_time in result:
-        print(thread_index, start_time)
+    for thread, time in result:
+        print(thread, time)
 
 if __name__ == "__main__":
     main()
